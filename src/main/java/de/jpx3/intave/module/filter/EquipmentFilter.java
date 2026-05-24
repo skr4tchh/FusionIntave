e8839afc3e82348643a4cac8feefcd008fcfec98
Skr4tchh <skr4tchh@gmail.com>
2026-05-24 23:34:40 +0300

    Final bug fixes in Filters

--- src/main/java/de/jpx3/intave/module/filter/EquipmentFilter.java
+++ src/main/java/de/jpx3/intave/module/filter/EquipmentFilter.java
@@ -6,6 +6,7 @@ import com.comphenix.protocol.reflect.StructureModifier;
 import com.comphenix.protocol.wrappers.EnumWrappers;
 import com.comphenix.protocol.wrappers.Pair;
 import de.jpx3.intave.IntavePlugin;
+import de.jpx3.intave.adapter.MinecraftVersions;
 import de.jpx3.intave.module.linker.packet.PacketSubscription;
 import de.jpx3.intave.packet.reader.EntityMetadataReader;
 import de.jpx3.intave.packet.reader.EntityReader;
@@ -76,85 +77,9 @@ public final class EquipmentFilter extends Filter {
         return itemStack;
     }
 
-  /*
-  @PacketSubscription(
-    packetsOut = {
-      ENTITY_EQUIPMENT
+    @Override
+    protected boolean enabled() {
+        return MinecraftVersions.VER1_21_4.atOrAbove() && super.enabled();
     }
-  )
-  public void filterEquipment(PacketEvent event) {
-    PacketContainer packet = event.getPacket();
-
-    if (packet.getItemModifier().readSafely(0) != null) {
-      // 1.8 - 1.15
-      ItemStack itemStack = packet.getItemModifier().readSafely(0);
-      ItemStack newItemStack = stripFromData(itemStack);
-      packet.getItemModifier().write(0, newItemStack);
-//      int a = packet.getIntegers().read(0);
-//      int b = packet.getIntegers().read(1);
-//      System.out.println("New equipment: " + itemStack + " " + a + " " + b);
-    } else {
-      List<Pair<EnumWrappers.ItemSlot, ItemStack>> read = packet.getSlotStackPairLists().read(0);
-      for (Pair<EnumWrappers.ItemSlot, ItemStack> itemSlotItemStackPair : read) {
-        ItemStack itemStack = itemSlotItemStackPair.getSecond().clone();
-        ItemStack newItemStack = stripFromData(itemStack);
-        itemSlotItemStackPair.setSecond(newItemStack);
-      }
-    }
-  }
-
-  private ItemStack stripFromData(ItemStack itemStack) {
-    itemStack.setAmount(1);
-
-    if (itemStack.hasItemMeta()) {
-      ItemMeta meta = itemStack.getItemMeta();
-      if (meta.hasEnchants()) {
-        for (Enchantment enchantment : itemStack.getEnchantments().keySet()) {
-          itemStack.removeEnchantment(enchantment);
-        }
-        itemStack.addUnsafeEnchantment(Enchantment.THORNS, 1);
-      }
-
-      // taken from https://gist.github.com/dmulloy2/5d52ddbb89a1609dbea2
-      if (meta instanceof BookMeta) {
-        BookMeta bookMeta = (BookMeta) meta;
-        bookMeta.setTitle(null);
-        bookMeta.setPages(Collections.emptyList());
-        bookMeta.setAuthor(null);
-      } else if (meta instanceof EnchantmentStorageMeta) {
-        EnchantmentStorageMeta enchantmentStorageMeta = (EnchantmentStorageMeta) meta;
-        if (enchantmentStorageMeta.hasStoredEnchants()) {
-          for (Enchantment ench : enchantmentStorageMeta.getStoredEnchants().keySet()) {
-            enchantmentStorageMeta.removeStoredEnchant(ench);
-          }
-          enchantmentStorageMeta.addStoredEnchant(Enchantment.THORNS, 1, true);
-        }
-      } else if (meta instanceof FireworkEffectMeta) {
-        ((FireworkEffectMeta) meta).setEffect(null);
-      } else if (meta instanceof FireworkMeta) {
-        FireworkMeta fireworkMeta = (FireworkMeta) meta;
-        fireworkMeta.clearEffects();
-        fireworkMeta.setPower(0);
-      }
-      //
-
-      meta.setDisplayName("");
-      if (meta.getLore() != null) {
-        meta.setLore(Collections.emptyList());
-      }
-      meta.removeItemFlags(meta.getItemFlags().toArray(new ItemFlag[0]));
-    }
-    return itemStack;
-  }
-
-  @Override
-  protected boolean enabled() {
-//    if (MinecraftVersions.VER1_19.atOrAbove()) {
-//      return false;
-//    }
-//    return !IntaveControl.GOMME_MODE && super.enabled();
-    return false;
-  }
-   */
 
 }
--- src/main/java/de/jpx3/intave/module/filter/HealthFilter.java
+++ src/main/java/de/jpx3/intave/module/filter/HealthFilter.java
@@ -10,10 +10,7 @@ import de.jpx3.intave.module.linker.packet.ListenerPriority;
 import de.jpx3.intave.module.linker.packet.PacketSubscription;
 import de.jpx3.intave.packet.reader.EntityMetadataReader;
 import de.jpx3.intave.packet.reader.PacketReaders;
-import org.bukkit.entity.EnderDragon;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
-import org.bukkit.entity.Wither;
+import org.bukkit.entity.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,91 +19,51 @@ import static de.jpx3.intave.module.linker.packet.PacketId.Server.ENTITY_METADAT
 
 public final class HealthFilter extends Filter {
 
-  private final IntavePlugin plugin;
+    private final IntavePlugin plugin;
 
-  private final int HEALTH_METADATA_INDEX = 9;
-
-  public HealthFilter(IntavePlugin plugin) {
-    super("health");
-    this.plugin = plugin;
-  }
-
-  @PacketSubscription(
-    packetsOut = {
-      ENTITY_METADATA
-    },
-    priority = ListenerPriority.NORMAL
-  )
-  public void depriveHealth(PacketEvent event) {
-      PacketContainer packet = event.getPacket();
-      int entityId = packet.getIntegers().read(0);
-      if (event.getPlayer().getEntityId() != entityId) {
-          List<WrappedDataValue> dataValues = event.getPacket().getDataValueCollectionModifier().read(0);
-          boolean shouldPush = false;
-
-          for (int i = 0; i < dataValues.size(); i++) {
-              WrappedDataValue dataValue = dataValues.get(i);
-              if (dataValue.getIndex() == HEALTH_METADATA_INDEX && dataValue.getRawValue() instanceof Float && (float) dataValue.getRawValue() != 0.0F) {
-                  dataValues.set(i, (new WrappedDataValue(
-                          dataValue.getIndex(),
-                          dataValue.getSerializer(),
-                          createFakeHealth()
-                  )));
-                  shouldPush = true;
-              }
-          }
-
-          if (shouldPush)
-              event.getPacket().getDataValueCollectionModifier().write(0, dataValues);
-      }
-  }
-
-  private float createFakeHealth() {
-      return Math.max(1, (float) (Math.random() * 20.0F));
-  }
-
-    /*
-  @PacketSubscription(
-    packetsOut = {
-      ENTITY_METADATA
-    },
-    priority = ListenerPriority.NORMAL
-  )
-  public void depriveHealth(PacketEvent event) {
-    event.setPacket(event.getPacket().deepClone());
-    PacketContainer packet = event.getPacket();
-    EntityMetadataReader reader = PacketReaders.readerOf(packet);
-    Entity entity = reader.entityBy(event);
-    if (entity == null || entity instanceof EnderDragon || entity instanceof Wither) {
-      reader.release();
-      return;
+    public HealthFilter(IntavePlugin plugin) {
+        super("health");
+        this.plugin = plugin;
     }
-    List<WrappedWatchableObject> watchables = reader.legacyMetadataObjects();
-    if (entity instanceof LivingEntity && entity.getEntityId() != event.getPlayer().getEntityId()) {
-      if (watchables != null) {
-        for (int i = 0; i < watchables.size(); i++) {
-          WrappedWatchableObject watchable = watchables.get(i);
-          if (watchable.getIndex() == 9 && watchable.getValue() instanceof Float) {
-            watchable = new WrappedWatchableObject(watchable.getIndex(), watchable.getRawValue());
-            stripHealthFrom(watchable);
-            watchables.set(i, watchable);
-          }
+
+    @PacketSubscription(
+            packetsOut = {
+                    ENTITY_METADATA
+            },
+            priority = ListenerPriority.NORMAL
+    )
+    public void depriveHealth(PacketEvent event) {
+        PacketContainer packet = event.getPacket();
+        EntityMetadataReader reader = PacketReaders.readerOf(packet);
+        Entity entity = reader.entityBy(event);
+        if (entity == null || !entity.getType().isAlive() || entity instanceof EnderDragon || entity instanceof Wither || event.getPlayer().getEntityId() == entity.getEntityId()) {
+            reader.release();
+            return;
         }
-      }
-    }
-    reader.setLegacyMetadataObjects(watchables);
-    reader.release();
-  }
 
-  private void stripHealthFrom(WrappedWatchableObject watchable) {
-    if (watchable != null && watchable.getIndex() == 6 && watchable.getRawValue() instanceof Float && (float) watchable.getRawValue() != 0.0F) {
-      watchable.setValue(createFakeHealth());
-    }
-  }
+        List<WrappedDataValue> dataValues = event.getPacket().getDataValueCollectionModifier().read(0);
+        for (int i = 0; i < dataValues.size(); i++) {
+            WrappedDataValue dataValue = dataValues.get(i);
+            if (dataValue.getIndex() == 9) {
+                dataValues.set(i, (new WrappedDataValue(
+                        dataValue.getIndex(),
+                        dataValue.getSerializer(),
+                        createFakeHealth()
+                )));
+            }
+        }
 
-  private float createFakeHealth() {
-    return Math.max(1, (float) (Math.random() * 20.0F));
-  }
-     */
+        event.getPacket().getDataValueCollectionModifier().write(0, dataValues);
+        reader.release();
+    }
+
+    private float createFakeHealth() {
+        return Math.max(1, (float) (Math.random() * 20.0F));
+    }
+
+    @Override
+    protected boolean enabled() {
+        return MinecraftVersions.VER1_21_4.atOrAbove() && super.enabled();
+    }
 
 }
