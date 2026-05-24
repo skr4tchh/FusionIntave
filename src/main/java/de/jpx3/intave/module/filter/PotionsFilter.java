e50d1d175d935f551da9a3d677c225c059f2657b
Skr4tchh <skr4tchh@gmail.com>
2026-05-24 23:40:25 +0300

    Add PotionsFilter version check

--- src/main/java/de/jpx3/intave/module/filter/PotionsFilter.java
+++ src/main/java/de/jpx3/intave/module/filter/PotionsFilter.java
@@ -2,6 +2,7 @@ package de.jpx3.intave.module.filter;
 
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
+import de.jpx3.intave.adapter.MinecraftVersions;
 import de.jpx3.intave.module.linker.packet.PacketSubscription;
 
 import static de.jpx3.intave.module.linker.packet.PacketId.Server.ENTITY_EFFECT;
@@ -23,6 +24,11 @@ public class PotionsFilter extends Filter {
         int entityId = packet.getIntegers().read(0);
         if (event.getPlayer().getEntityId() != entityId)
             event.setCancelled(true);
+    }
+
+    @Override
+    protected boolean enabled() {
+        return MinecraftVersions.VER1_21_4.atOrAbove() && super.enabled();
     }
 
 }
